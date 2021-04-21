414f1f5800ed42d1652907420d1185bae5f8c6a2
Jean-Yves Didier <jean-yves.didier@ibisc.univ-evry.fr>
2021-04-21 14:33:51 +0200

    created a test directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/xrsystem.js',
+    mode: 'production',
     output: {
         filename: 'webar-polyfill.js',
         path: path.resolve(__dirname, 'dist'),
